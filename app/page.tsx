feab93e7c3a3d310671976d16160cfda911f08a6
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-12-17 19:06:38 +0530

    Add Hero component and update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
     return (
-        <main>
-            <h1>Home</h1>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        <main className='overflow-hidden'>
+            <Hero />
         </main>
     )
 }
