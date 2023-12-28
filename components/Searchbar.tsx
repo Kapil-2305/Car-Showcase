3978dd3d6e97de5e4acbf2149d4a1893c83bdade
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-12-28 23:52:26 +0530

    Refactor updateSearchParams function in SearchBar component

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -35,7 +35,6 @@ const SearchBar = () => {
     };
 
     const updateSearchParams = (model: string, manufacturer: string) => {
-        // Create a new URLSearchParams object using the current URL search parameters
         const searchParams = new URLSearchParams(window.location.search);
 
         // Update or delete the 'model' search parameter based on the 'model' value
