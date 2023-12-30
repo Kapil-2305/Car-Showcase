017dcef0d3d3e9fb51713c4ad990445a9caf8f1d
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-12-30 15:53:12 +0530

    Refactor CarDetails component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Fragment } from "react";
 import Image from "next/image";
 
@@ -78,14 +80,14 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
 
                                     <div className='mt-3 flex flex-wrap gap-4'>
                                         {Object.entries(car).map(([key, value]) => (
-                                        <div className='flex justify-between gap-5 w-full text-right' key={key} >
-                                            <h4 className='text-grey capitalize'>
-                                            {key.split("_").join(" ")}
-                                            </h4>
-                                            <p className='text-black-100 font-semibold'>
-                                            {value}
-                                            </p>
-                                        </div>
+                                            <div className='flex justify-between gap-5 w-full text-right' key={key} >
+                                                <h4 className='text-grey capitalize'>
+                                                    {key.split("_").join(" ")}
+                                                </h4>
+                                                <p className='text-black-100 font-semibold'>
+                                                    {value}
+                                                </p>
+                                            </div>
                                         ))}
                                     </div>
                                 </div>
